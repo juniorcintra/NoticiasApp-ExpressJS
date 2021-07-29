370b8af342e39f6c8948be5fc7ba4d4ca9be5aae
Junior Cintra <junior.developer@outlook.com.br>
2021-07-29 07:09:20 -0300

    implementando express validator

--- config/server.js
+++ config/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 var consign = require("consign");
 var bodyParser = require("body-parser");
+var validator = require("express-validator");
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "./app/views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(validator());
 
 consign()
   .include("app/routes")
