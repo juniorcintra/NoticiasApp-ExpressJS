ebe04eb57e26ec63ebf97d9af66209d189dfeb0c
Junior Cintra <junior.developer@outlook.com.br>
2021-07-29 05:47:49 -0300

    refactoring com consign

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,9 +1,6 @@
-var dbConnection = require("../../config/database");
-
 module.exports = function (app) {
-  var connection = dbConnection();
-
   app.get("/noticias", function (req, res) {
+    var connection = app.config.database();
     connection.query("select * from noticias", function (error, result) {
       res.render("noticias/noticias", { noticias: result });
     });
--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const app = require("./config/server");
 
-// const rotaNoticicas = require("./app/routes/noticias");
-// rotaNoticicas(app);
-
-// const rotaHome = require("./app/routes/home");
-// rotaHome(app);
-
-// const rotaForm = require("./app/routes/formulario_noticia");
-// rotaForm(app);
-
 app.listen(4000, function () {
   console.log("Servidor rodando com Express");
 });
