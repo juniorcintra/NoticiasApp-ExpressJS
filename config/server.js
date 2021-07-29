23bd0a46caeda9ddcdec6755d3a49a7398c37571
Junior Cintra <junior.developer@outlook.com.br>
2021-07-29 05:38:53 -0300

    instalacao de configuracao do consign

--- config/server.js
+++ config/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+var consign = require("consign");
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "./app/views");
+
+consign().include("app/routes").into(app);
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const app = require("./config/server");
 
-const rotaNoticicas = require("./app/routes/noticias");
-rotaNoticicas(app);
+// const rotaNoticicas = require("./app/routes/noticias");
+// rotaNoticicas(app);
 
-const rotaHome = require("./app/routes/home");
-rotaHome(app);
+// const rotaHome = require("./app/routes/home");
+// rotaHome(app);
 
-const rotaForm = require("./app/routes/formulario_noticia");
-rotaForm(app);
+// const rotaForm = require("./app/routes/formulario_noticia");
+// rotaForm(app);
 
 app.listen(4000, function () {
   console.log("Servidor rodando com Express");
