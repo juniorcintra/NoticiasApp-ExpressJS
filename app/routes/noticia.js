427b866d13f85be1581208449c612cae8c5989c2
Junior Cintra <junior.developer@outlook.com.br>
2021-07-29 06:12:12 -0300

    implementacao de models

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -1,11 +1,10 @@
 module.exports = function (app) {
   app.get("/noticia", function (req, res) {
     var connection = app.config.database();
-    connection.query(
-      "select * from noticias where id_noticia = 2",
-      function (error, result) {
-        res.render("noticias/noticia", { noticia: result });
-      }
-    );
+    var noticiasModel = app.app.models.noticiasModel();
+
+    noticiasModel.getNoticia(connection, function (error, result) {
+      res.render("noticias/noticia", { noticia: result });
+    });
   });
 };
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,7 +1,9 @@
 module.exports = function (app) {
   app.get("/noticias", function (req, res) {
     var connection = app.config.database();
-    connection.query("select * from noticias", function (error, result) {
+    var noticiasModel = app.app.models.noticiasModel();
+
+    noticiasModel.getNoticias(connection, function (error, result) {
       res.render("noticias/noticias", { noticias: result });
     });
   });
